e8cec0460a58aaa0ba4d801a7454bcf3e0180629
ciceroinfo <cicero.info@gmail.com>
2014-06-30 22:21:09 -0300

    correção

--- assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices/src/edu/vuum/mocca/DownloadIntentService.java
+++ assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices/src/edu/vuum/mocca/DownloadIntentService.java
@@ -87,7 +87,7 @@ public class DownloadIntentService extends IntentService {
         // in the intent and returns the file's pathname using a
         // Messenger who's Bundle key is defined by DownloadUtils.MESSENGER_KEY
     	
-		String pathname = intent.getData().toString();
+		String pathname = DownloadUtils.downloadFile(this, intent.getData());
 
 		Messenger messenger = (Messenger) intent.getExtras().get(
 				DownloadUtils.MESSENGER_KEY);
